5db21feba1f81a2b2cfd5e8249f6480f66d3f755
Sam Thursfield <ssssam@gmail.com>
2009-07-03 17:36:24 +0100

    Re-add UNMANGLED_PATH to config.py ..

--- jhbuild/config.py
+++ jhbuild/config.py
@@ -159,6 +159,7 @@ class Config:
 
         if not self._orig_environ:
             self.__dict__['_orig_environ'] = os.environ.copy()
+            os.environ['UNMANGLED_PATH'] = os.environ.get('PATH', '')
 
         try:
             SRCDIR
